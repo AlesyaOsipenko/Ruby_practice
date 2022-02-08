52ffba6354de2012dfbf57d28f9ee1542b636300
Alesya Osipenko <alesya-osipenko@mail.ru>
2022-02-08 17:04:08 +0300

    finished token/random game

--- lesson7/task7_6.rb
+++ lesson7/task7_6.rb
@@ -1,4 +1,21 @@
-# comp_number = rand(15)
-# puts "Загадано число от 0 до 15, отгадайте какое? "
-# user_number = gets.to_i
+computer_number = rand(16)
+puts "Загадано число от 0 до 15, отгадайте какое?"
 
+user_number = nil
+
+3.times do
+    user_number = gets.to_i
+
+        break if user_number == computer_number
+
+    hot_or_not = (user_number - computer_number).abs <=2 ? "Тепло" : "холодно" 
+    more_or_less = user_number > computer_number ? "меньше" : "больше"
+
+    puts "#{hot_or_not} (нужно #{more_or_less})"
+end
+
+if user_number == computer_number 
+    puts "ура, вы выиграли!"
+else
+    puts "Вы не угадали, загадано было #{computer_number}"
+end
