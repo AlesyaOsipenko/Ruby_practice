63bc6e3f7bcb93205c08aaaf1042f7d17d285c0c
Alesya Osipenko <alesya-osipenko@mail.ru>
2022-02-13 15:23:11 +0300

    start learning methods

---
+++ ruby_methods/methods.rb
@@ -0,0 +1,13 @@
+#Объявление метода
+def greeting_text(name)
+    return "Привет, #{name}!"
+end
+
+#--
+
+#Вызов метода
+text1 = greeting_text("Алеся")
+puts text1
+
+text2 = greeting_text("Миша")
+puts text2
